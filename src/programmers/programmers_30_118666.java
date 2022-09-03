88a5ff2f204d5659969249d9e6e947ca0416b909
zmfkzj <qlwlal@naver.com>
2022-09-03 22:51:52 +0900

    programmers_30_118666 getOrDefault 사용하는 방식으로 리펙토링

--- src/programmers/programmers_30_118666.java
+++ src/programmers/programmers_30_118666.java
@@ -6,15 +6,8 @@ import java.util.HashMap;
 public class programmers_30_118666 {
     public String solution(String[] survey, int[] choices) {
         HashMap<String, Integer> score = new HashMap<>();
-        score.put("R",0);
-        score.put("T",0);
-        score.put("C",0);
-        score.put("F",0);
-        score.put("J",0);
-        score.put("M",0);
-        score.put("A",0);
-        score.put("N",0);
         int[] scores = new int[]{3,2,1,0,1,2,3};
+
         for (int i = 0; i < survey.length; i++) {
             String s = survey[i];
             String s1 = s.substring(0,1);
@@ -22,19 +15,19 @@ public class programmers_30_118666 {
             String[] types = new String[]{s1, s1, s1, s1, s2, s2, s2};
 
             int c = choices[i];
-            score.put(types[c-1], score.get(types[c-1])+scores[c-1]);
+            score.put(types[c-1], score.getOrDefault(types[c-1],0)+scores[c-1]);
         }
         String base_result = "RCJA";
-        if(score.get("R")<score.get("T")){
+        if(score.getOrDefault("R",0)<score.getOrDefault("T",0)){
             base_result = base_result.replace("R","T");
         }
-        if(score.get("C")<score.get("F")){
+        if(score.getOrDefault("C",0)<score.getOrDefault("F",0)){
             base_result = base_result.replace("C","F");
         }
-        if(score.get("J")<score.get("M")){
+        if(score.getOrDefault("J",0)<score.getOrDefault("M",0)){
             base_result = base_result.replace("J","M");
         }
-        if(score.get("A")<score.get("N")){
+        if(score.getOrDefault("A",0)<score.getOrDefault("N",0)){
             base_result = base_result.replace("A","N");
         }
         return base_result;
